5a8bbf8e5a8c8a3cf741ea7a99e8ea61cb222554
Yulya-Z <zukovayulya@gmail.com>
2024-12-11 23:12:58 +0300

    Add files via upload

---
+++ module_10_3.py
@@ -0,0 +1,40 @@
+import threading
+import random
+import time
+
+class Bank:
+    def __init__(self):
+        self.balance = 0
+        self.lock = threading.Lock()
+
+    def deposit(self):
+        for i in range(100):
+            ran_num = random.randint(50, 500)
+            self.balance += ran_num
+            print(f"Пополнение: {ran_num}. Баланс: {self.balance}")
+            if self.balance >= 500 and self.lock.locked():
+                self.lock.release()
+            time.sleep(0.001)
+
+    def take(self):
+        for i in range(100):
+            ran_num = random.randint(50, 500)
+            print(f"Запрос на {ran_num}")
+            if ran_num <= self.balance:
+                self.balance -= ran_num
+                print(f"Снятие: {ran_num}. Баланс: {self.balance}")
+            else:
+                print("Запрос отклонён, недостаточно средств")
+                self.lock.acquire()
+            time.sleep(0.001)
+
+bk = Bank()
+th1 = threading.Thread(target=Bank.deposit, args=(bk,))
+th2 = threading.Thread(target=Bank.take, args=(bk,))
+
+th1.start()
+th2.start()
+th1.join()
+th2.join()
+
+print(f'Итоговый баланс: {bk.balance}')
