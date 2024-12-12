779234fe5cbeed04268d1b72daddb82d3ff83c62
Yulya-Z <zukovayulya@gmail.com>
2024-12-12 20:46:18 +0300

    Update module_10_3.py

--- module_10_3.py
+++ module_10_3.py
@@ -14,7 +14,7 @@ class Bank:
             print(f"Пополнение: {ran_num}. Баланс: {self.balance}")
             if self.balance >= 500 and self.lock.locked():
                 self.lock.release()
-            time.sleep(0.001)
+        time.sleep(0.001)
 
     def take(self):
         for i in range(100):
